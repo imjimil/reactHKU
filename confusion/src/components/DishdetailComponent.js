ceecaf271e8fce9d1afb18a91641b61601be6b1a
abc <prajapatijimil@yahoo.com>
2020-05-18 21:45:25 +0530

    fix the menu click issue also put some breadcrumb to pages

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
   function RenderComments({ comments }){
@@ -67,9 +66,23 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     }
     return (
       <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to={"/menu"}>Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>
+              {props.dish.name}
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
       <div className="row">
         <RenderDish dish= {props.dish} />
-        <RenderComments comments = {props.dish.comments} />
+        <RenderComments comments = {props.comments} />
       </div>
       </div>
     );
