be9c6a6a10e1bc7e62971dbd3b8d12b31c1a5ae2
abc <prajapatijimil@yahoo.com>
2020-05-23 11:23:16 +0530

    assignment 4 completed

--- confusion/src/components/HomeComponent.js
+++ confusion/src/components/HomeComponent.js
@@ -52,7 +52,7 @@ function Home(props) {
                     <RenderCard item={props.promotion} isLoading = { props.promosLoading } errMess = { props.promosErrMess } />
                 </div>
                 <div className="col-12 md-1 col-md">
-                    <RenderCard item={props.leader} />
+                    <RenderCard item={props.leader} isLoading={props.leadersLoading} errMess={props.leadersErrMess} />
                 </div>
             </div>
         </div>
--- confusion/src/redux/ActionCreators.js
+++ confusion/src/redux/ActionCreators.js
@@ -42,6 +42,46 @@ export const postComment = (dishId, rating, author, comment) => (dispatch) => {
     .catch(error =>  { console.log('post comments', error.message); alert('Your comment could not be posted\nError: '+error.message); });
 };
 
+export const postFeedback = (feedback) => (dispatch) => {
+  const newFeedback = {
+     firstname: feedback.firstname,
+     lastname: feedback.lastname,
+     telnum: feedback.telnum,
+     email: feedback.email,
+     agree: feedback.agree,
+     contactType: feedback.contactType,
+     message: feedback.message
+  }
+   
+  return fetch(baseUrl + 'feedback', {
+     method: 'POST',
+     body: JSON.stringify(newFeedback),
+     headers: {
+        'Content-Type': 'application/json'
+     },
+     credentials: 'same-origin'
+  })
+  .then(response => {
+     if (response.ok) {
+        return response;
+     }
+     else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+     }
+  }, 
+  error => {
+     var errmess = new Error(error.message);    
+     throw errmess;
+  })
+  .then(response => response.json())
+  .then(response => { console.log("Current State is: " + JSON.stringify(response)); 
+       alert("Thank you for your feedback!<br/>" + JSON.stringify(response)); })
+  .catch(error => { console.log('Post comments ', error.message);
+       alert('Your feedback could not be posted\nError: ' + error.message); })
+}
+
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
     return fetch(baseUrl + 'dishes')
@@ -142,3 +182,40 @@ export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
 });
+
+export const fetchLeaders = () => (dispatch) => {
+  dispatch(leadersLoading(true));
+  
+  return fetch(baseUrl + 'leaders')
+  .then(response => {
+     if (response.ok) {
+        return response;
+     }
+     else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+     }
+  }, 
+  error => {
+     var errmess = new Error(error.message);
+     throw errmess;
+  })
+  .then(response => response.json())
+  .then(promos => dispatch(addLeaders(promos)))
+  .catch(error => dispatch(leadersFailed(error.message)));
+}
+
+export const leadersLoading = () => ({
+  type: ActionTypes.LEADERS_LOADING
+});
+
+export const leadersFailed = (errmess) => ({
+  type: ActionTypes.LEADERS_FAILED,
+  payload: errmess
+});
+
+export const addLeaders = (leaders) => ({
+  type: ActionTypes.ADD_LEADERS,
+  payload: leaders
+})
